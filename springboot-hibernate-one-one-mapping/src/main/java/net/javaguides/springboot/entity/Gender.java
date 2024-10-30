fe467490530fb83db5b01f566e748a56c1475ff1
KauanAltino <kauanaltino@gmail.com>
2024-10-30 09:02:53 -0300

    organizando

--- springboot-hibernate-one-one-mapping/src/main/java/net/javaguides/springboot/entity/Gender.java
+++
@@ -1,6 +0,0 @@
-package net.javaguides.springboot.entity;
-
-public enum Gender {
-	MALE, FEMALE
-
-}
--- springboot-mssql-jpa-hibernate-crud-example/src/main/java/net/javaguides/mssql/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package net.javaguides.mssql.repository;
-
-import net.javaguides.mssql.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
-
-}
